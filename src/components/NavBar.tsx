30b1e35aef8148ce1b7985b72c8c0223db84f5b5
Walid Kabboussa <walidkabou@gmail.com>
2023-09-09 15:30:32 +0100

    Add color mode controll button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { HStack, Image, Text } from "@chakra-ui/react"
+import { Button, HStack, Image, Text, useColorMode } from "@chakra-ui/react"
 import logo from '../assets/website logo.png'
 
 export const Navbar = () => {
+    const { colorMode, toggleColorMode } = useColorMode()
   return (
     <HStack>
         <Image src={logo} boxSize= '80px'  />
         <Text> Home </Text>
+        <Button onClick={toggleColorMode}>
+         {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
     </HStack>
   )
 }
