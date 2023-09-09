d40152d7ecc8cdf62c57c925b420f8d75185d3a4
Walid Kabboussa <walidkabou@gmail.com>
2023-09-09 22:50:15 +0100

    Install react-icons and place icons for dark & ligth mode

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,13 +1,13 @@
 import { HStack, Switch, useColorMode } from '@chakra-ui/react'
 import React from 'react'
-import { MdOutlineDarkMode } from 'react-icons/md'
+import { MdOutlineDarkMode, MdOutlineLightMode} from 'react-icons/md'
 
 export const ColorModeSwitch = () => {
     const { toggleColorMode, colorMode } = useColorMode()
   return (
     <HStack>
         <Switch colorScheme='green' isChecked = {colorMode === 'dark'} onChange={toggleColorMode} />
-         <MdOutlineDarkMode />
+         {colorMode === 'dark' ? <MdOutlineLightMode /> : <MdOutlineDarkMode />   }
     </HStack>
   )
 }
