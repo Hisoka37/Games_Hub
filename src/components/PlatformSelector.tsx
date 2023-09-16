95cd46751ccb58a06ee91912476bb90e73ebd755
Walid Kabboussa <walidkabou@gmail.com>
2023-09-16 14:38:23 +0100

    filter games by platfroms

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,17 +1,31 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import { AiOutlineDown } from 'react-icons/ai'
-import usePlatforms from '../hooks/usePlatforms'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { AiOutlineDown } from "react-icons/ai";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-export const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
+}
 
-    const { data, error } = usePlatforms();
-   if ( error ) return null;
+export const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+  if (error) return null;
   return (
     <Menu>
-        <MenuButton as={Button} rightIcon={<AiOutlineDown />}> Platofrms </MenuButton>
-        <MenuList>
-          {data.map(platfrom => <MenuItem key={platfrom.id}>{platfrom.name}</MenuItem>)}
-        </MenuList>
+      <MenuButton as={Button} rightIcon={<AiOutlineDown />}>
+        {selectedPlatform?.name || 'Platform'}
+      </MenuButton>
+      <MenuList>
+        {data.map((platfrom) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platfrom)}
+            key={platfrom.id}
+          >
+            {platfrom.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
-  )
-}
+  );
+};
